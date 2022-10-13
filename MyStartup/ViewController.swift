7e11b6288a956169997b74b5de14df123f3ae558
Ioannnis Pavlidis <ipavlidis>
2022-10-13 16:14:11 -0500

    Initial Commit

---
+++ MyStartup/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyStartup
+//
+//  Created by Ioannis Pavlidis on 10/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
